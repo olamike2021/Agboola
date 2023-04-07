ad782e41175a4d853e96abaed49860a80946f155
olamike2021 <agboolafolorunso4@gmail.com>
2023-04-07 09:26:56 +0200

    rebrand code for python file in master branch

---
+++ python1.py
@@ -0,0 +1,22 @@
+name = "michael"
+my_email = f"""
+hello {name},
+good morning 
+how are you doing?
+hope you slept well
+age = {12+12}
+pi = {3.14}
+numbers = {10, 20, 30, 40, 50, 60, 60, 70, 80, 90, 100}
+numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+"""
+print(10 > 5)
+print(10 >= 10)
+print(10 < 10)
+print(10 <= 10)
+print(10 != 10)
+print(10 == 10)
+print(3.0 < 3)
+
+print((10 > 5)
+      or 1 < 3
+      and "m" == "f")
